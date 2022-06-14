1ad1e213e013e9b39e8f1b50769115394e7fb083
Wes <wescarmstrong@gmail.com>
2022-06-14 00:55:43 -0500

    Style updates

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
+let uploadFileResults = {};
+let file = {};
+
 
 const startUpload = document.getElementById('startUpload').addEventListener('click', () => {
     Papa.parse(document.getElementById('uploadFile').files[0], 
     {
-        download: true,
         header: true,
         // skipEmptyLines: true,
-        complete: function(results) {
-            console.log(results);
+        complete: function(results, file) {
+            uploadFileResults = results;
+            file = file;
+            // Example push specific data: for loop -  for (i=0; i< results.data.length; i++) { x.push(results.data[i].productID); }
+            console.log(uploadFileResults);
+            console.log(file);
         }
     });
 });
